0f8dcae20ee5d631bb0907da1af2143e70429f96
marota <antoine-marot@live.fr>
2022-01-14 14:27:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='grid2viz-dataset-neurips-robustness',
+   name='Grid2viz-dataset-NeurIPS-Robustness_dec12_1',
    version='1.0',
    description='A useful module',
    author='Man Foo',
    author_email='foomail@foo.com',
-   packages=['grid2viz-dataset-neurips'],  #same as name
+   packages=['Grid2viz-dataset-NeurIPS-Robustness_dec12_1'],  #same as name
    install_requires=['grid2viz-dataset-neurips', 'jupyter-server-proxy', 'jupyter-dash','grid2viz'], #external packages as dependencies
 )
